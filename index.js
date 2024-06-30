4d1ac29fa67e07a959614d35e45df98705aff194
carlosfermed <harmonium_skies@hotmail.com>
2024-06-30 15:26:16 +0200

    Servicio de cuentas '/account' creado

--- index.js
+++ index.js
@@ -11,26 +11,63 @@ const expressApp = express();
 expressApp.use(express.json());
 expressApp.use(express.text());
 
-//Obtener detalles
-expressApp.get("/account/:guid", (req, res) => {
+//Obtener datos genéricos de todos los usuarios
+expressApp.get("/account", (req, res) => {
     
-    const userData = {
-        id: USERS_BBDD[req.params.guid]._id,
-        name: USERS_BBDD[req.params.guid].name,
-    }
+    const usersData = USERS_BBDD.map(user => ({
+        id: user._id,
+        name: user.name,
+        email: user.email,  
+    }))
+    res.send(usersData);
+})
+//Obtener detalles de usuario concreto
+expressApp.get("/account/:guid", (req, res) => {    
+    const userData = USERS_BBDD.find(user => user._id == req.params.guid);
+
+    if (!userData)
+        return res.status(404).send("Usuario no encontrado.");
+
     res.send(userData);
 })
 //Crear nueva cuenta
 expressApp.post("/account", (req, res) => {
+    const {guid, name, email} = req.body;
+
+    if (!guid || !name || !email)
+        return res.status(400).send("Los datos están incompletos o son incorrectos.")
+
+    const userData = USERS_BBDD.find(user => user._id == req.params.guid);
+
+    if (userData)
+        return res.status(409).send("El usuario ya existe.")
+
+    USERS_BBDD.push({_id: guid, name, email});
+
+    res.send("Usuario creado.");
 
 })
 //Actualizar cuenta
-expressApp.patch("/account", (req, res) => {
+expressApp.patch("/account/:guid", (req, res) => {
+    const userData = USERS_BBDD.find(user => user._id == req.params.guid);
 
+    if (!userData)
+        return res.status(404).send("Usuario no existe.");
+    
+    const userIndex = USERS_BBDD.findIndex(user => user._id == req.params.guid);
+
+    const patchedUser = {...userData, ...req.body};
+    USERS_BBDD.splice(userIndex, 1, patchedUser);
+
+    res.status(201).send(patchedUser);
 })
 //Eliminar cuenta
-expressApp.delete("/account", (req, res) => {
+expressApp.delete("/account/:guid", (req, res) => {
+    const userIndex = USERS_BBDD.findIndex(user => user._id == req.params.guid);
 
+    USERS_BBDD.splice(userIndex, 1);
+    console.log(USERS_BBDD);
+    res.status(200).send("Usuario eliminado.");
 })
 
 
